3bce1563703dd1acd8649ac334b5f4e2a15463ff
Timoofey Kuznetsov <tima1702@gmail.com>
2023-10-03 01:01:29 +0300

    Fix Test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Fake App Test/i);
-  // const linkElement = screen.getByText(/My awesome React Project/i);
+  // const linkElement = screen.getByText(/Fake App Test/i);
+  const linkElement = screen.getByText(/My awesome React Project/i);
   expect(linkElement).toBeInTheDocument();
 });
