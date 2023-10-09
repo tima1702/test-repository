2b4f6998cc91af661b2dddfaba2f974cb458eb8c
NikrOKvillon <kiradmitrev19861101ar@gmail.com>
2023-10-09 15:23:40 +0300

    Кононов К.Д.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Кононов К.Д. (02.03.03 - АИСа - о20)";
 
   console.log('This is App ');
   return (
