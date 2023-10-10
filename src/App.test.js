e8c2d9f8fd11da3fab7d7e03454c6ec856647411
tima1702 <tima1702@users.noreply.github.com>
2023-10-10 05:11:29 +0000

    deploy: c726878ba07f86ad81dd93688e1b536891bdbfbe

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders greeting message', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/Fake App Test/i);
-  const linkElement = screen.getByText(/Это тестовый проект/i);
-  expect(linkElement).toBeInTheDocument();
-});
