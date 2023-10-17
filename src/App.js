99ca37d00465d0dfeb536104ebb8addd07c39693
Никита <nik.zamula.01@mail.ru>
2023-10-17 17:18:34 +0300

    Замула Н.С.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Замула Н.С. (02.03.03 - АИСа - о20";
 
   console.log('This is App ');
   return (
