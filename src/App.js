60089892d7e82610e61f4db736e59cb8679cb0c8
alena-sergienko <148280306+alena-sergienko@users.noreply.github.com>
2023-11-25 22:24:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Сергиенко Алёны Демьяновны ";
 
-  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
