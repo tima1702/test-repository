57739fbf05fed6a2a92eb4713195c28f017112e6
Nikerty <nikertylitik@gmail.com>
2023-11-27 22:08:50 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 function App() {
   const greeting = "Это тестовый проект ФИО";
-
-  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
