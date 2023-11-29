f0e813f5631886e2d5895290b13b4cbffdd28956
Mefix69 <102327117+Mefix69@users.noreply.github.com>
2023-11-29 20:04:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Ситулиной Надежды Игоревны";
+  const greeting = "Это тестовый проект Ситулиной Надежды Игоревны tete";
 
   // console.log('This is App ');
   return (
