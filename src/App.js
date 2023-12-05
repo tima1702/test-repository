b1d02f16785a9b7c7263a002a3e251a9b3870d0d
Al_tair <alnazzars2003@gmail.com>
2023-12-05 16:06:53 +0300

    5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Аль Нажжар Сайфальдин";
 
-  console.log('This is App ');
+  
   return (
     <div className="App">
      <header className="App-header">
