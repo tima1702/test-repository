2710a672e3f39cdf6f01534a1c7185272bfe3d47
Al_tair <alnazzars2003@gmail.com>
2023-12-05 15:43:33 +0300

    3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Аль Нажжар Сайфальдин";
+  const greeting = "Это тестовый проект Аль Нажжар Сайфальдина";
 
   console.log('This is App ');
   return (
