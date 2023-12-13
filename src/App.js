984c833e836f9e19bf6cdfde507726efef4efc17
nikitoxxxyk <148775620+nikitoxxxyk@users.noreply.github.com>
2023-12-13 21:29:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Никиты Купрысь";
+  const greeting = "Это тестовый проект Никиты Купрысь ";
 
   return (
     <div className="App">
