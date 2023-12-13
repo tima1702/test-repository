8e022d5d62d5048576f9a960ffd496365d781f85
Mefix69 <nadyasitulina@gmail.com>
2023-12-13 21:12:06 +0300

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Никиты Купрысь";
 
-  // console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
