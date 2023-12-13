e7ea137bb5c320690b50a0dcfe8a4e82df8811cd
Danil101 <96428001+Danil101@users.noreply.github.com>
2023-12-13 04:43:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Михайличенко Данила Максимович";
+  const greeting = "";
 
   console.log('This is App ');
   return (
