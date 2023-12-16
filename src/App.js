d3c39cfcf9702ed3fce2d3236eb88e5389c5c3d9
AlexCatoria <catoria@ya.ru>
2023-12-16 20:24:21 +0300

    Removing errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Михайловского Александра Александровича";
 
-  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
